7ff8a74f77cd277f4d97ccc192d4bc2ba1588e46
PrataCat <111517381+PrataCat@users.noreply.github.com>
2023-05-27 13:23:00 +0000

    Deploying to gh-pages from @ PrataCat/goit-react-hw-08-phonebook@cf6de64b7adbe35c2617e6a873b4983a2ef6adfb 🚀

---
+++ static/js/35.4652c56a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[35],{2035:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var o=t(184),c=function(){return(0,o.jsxs)("h1",{children:["Wellcome to the ",(0,o.jsx)("span",{children:"CONTACTS APP"})]})}}}]);
+//# sourceMappingURL=35.4652c56a.chunk.js.map
